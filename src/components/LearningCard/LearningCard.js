23bcb52523bc64047eda9723d31af75e21b10846
Prashant Andani <prashant.andani@ymedialabs.com>
2019-10-08 18:59:43 +0530

    Add speech

--- src/components/LearningCard/LearningCard.js
+++ src/components/LearningCard/LearningCard.js
@@ -4,8 +4,20 @@ import './LearningCard.scss';
 
 const LearningCard = ({ title, image }) => {
   const imgUrl = `${image}?tr=w-200,h-200`;
+  const msg = new SpeechSynthesisUtterance();
+
+  const speak = () => {
+    speechSynthesis.getVoices().forEach(voice => {
+      if (voice.name === 'Samantha') {
+        msg.voice = voice;
+      }
+    });
+    msg.text = title;
+
+    window && window.speechSynthesis.speak(msg);
+  };
   return (
-    <section className="learning-card">
+    <section className="learning-card" role="click" onClick={speak}>
       <span className="title">{title}</span>
       <div>
         <img loading="lazy" src={imgUrl} className="img" alt={title} />
